0e6665b48f635f9f60285d4e160316093da37708
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-02-10 12:03:37 -0700

    added cards list

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Card = () => {
+const Card = ({name, email, id}) => {
   return (
-    <div>
-      <img alt='robots' src='' />
-      <h2>Jane Doe</h2>
-      <p>jane.doe@gmail.com</p>
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default Card;
